9835bc7bae57d7671111d49dd6735fe996e92d91
tgirotto <YOUR EMAIL ADDRESS>
2016-03-04 13:24:21 +0800

    before checkout

---
+++ Author.py
@@ -0,0 +1,7 @@
+#
+# from google.appengine.ext import ndb
+#
+# class Author(ndb.Model):
+#     """Sub model for representing an author."""
+#     identity = ndb.StringProperty(indexed=False)
+#     email = ndb.StringProperty(indexed=False)
---
+++ Greeting.py
@@ -0,0 +1,9 @@
+# from google.appengine.ext import ndb
+#
+# import Author
+#
+# class Greeting(ndb.Model):
+#     """A main model for representing an individual Guestbook entry."""
+#     author = ndb.StructuredProperty(Author)
+#     content = ndb.StringProperty(indexed=False)
+#     date = ndb.DateTimeProperty(auto_now_add=True)
